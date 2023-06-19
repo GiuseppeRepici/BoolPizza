963ea40ccaea96b8e9e682f8c139d6a16c56e956
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-06-19 17:30:32 +0200

    modifiche sfondo

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <form action="{{ route('pizzas.store') }}" method="POST">
+    <div class="wrapper">
+        <div class="container">
+            <form action="{{ route('pizzas.store') }}" method="POST">
 
-            @csrf
-            <div class="form-group">
-                <label for="name">Nome Pizza</label>
-                <input type="text" name="name" class="form-control" id="name">
-            </div>
-            <div class="form-group">
-                <label for="type">Tipo Pizza</label>
-                <input type="text" name="type" class="form-control" id="type">
-            </div>
-            <div class="form-group">
-                <label for="ingredients">Ingredienti</label>
-                <input type="text" name="ingredients" class="form-control" id="ingredients">
-            </div>
-            <div class="form-group">
-                <label for="price">Prezzo</label>
-                <input type="number" min="0.00" max="99.99" step="0.01" name="price" class="form-control"
-                    id="price">
-            </div>
-            <button class="btn btn-primary" type="submit">Invia</button>
-        </form>
+                @csrf
+                <div class="form-group">
+                    <label for="name" style="color: white;">Nome Pizza</label>
+                    <input type="text" name="name" class="form-control" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="type" style="color: white;">Tipo Pizza</label>
+                    <input type="text" name="type" class="form-control" id="type">
+                </div>
+                <div class="form-group">
+                    <label for="ingredients" style="color: white;">Ingredienti</label>
+                    <input type="text" name="ingredients" class="form-control" id="ingredients">
+                </div>
+                <div class="form-group">
+                    <label for="price" style="color: white;">Prezzo</label>
+                    <input type="number" min="0.00" max="99.99" step="0.01" name="price" class="form-control"
+                        id="price">
+                </div>
+                <button class="btn btn-primary" type="submit">Invia</button>
+            </form>
+        </div>
     </div>
 @endsection
--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -1,14 +1,16 @@
 @extends ('layouts.app')
 @section('content')
-    <div class="container">
-        <div class="card m-3">
-            <div class="card-body">
-                <h5 class="card-title">Pizza: {{ $Pizza->name }}</h5>
-                <p class="card-text">Tipologia: {{ $Pizza->type }}</p>
-                <p class="card-text">Ingredienti: {{ $Pizza->ingredients }}</p>
-                <p class="card-text">Prezzo: {{ $Pizza->price }} €</p>
-                <a href="{{ route('pizzas.index') }}">Torna alla home</a>
-                <a href="{{ route('pizzas.edit', $Pizza->id) }}">modifica</a>
+    <div class="wrapper">
+        <div class="container">
+            <div class="card m-3">
+                <div class="card-body">
+                    <h5 class="card-title">Pizza: {{ $Pizza->name }}</h5>
+                    <p class="card-text">Tipologia: {{ $Pizza->type }}</p>
+                    <p class="card-text">Ingredienti: {{ $Pizza->ingredients }}</p>
+                    <p class="card-text">Prezzo: {{ $Pizza->price }} €</p>
+                    <a href="{{ route('pizzas.index') }}">Torna alla home</a>
+                    <a href="{{ route('pizzas.edit', $Pizza->id) }}">modifica</a>
+                </div>
             </div>
         </div>
     </div>
